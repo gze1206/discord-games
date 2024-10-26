bc256b6ed75bdfe1889676a66ea62bc8e0a8f3a5
gze1206 <gze1206@naver.com>
2024-10-27 00:07:43 +0900

    feat: Apply IMessageHandler to Test script

--- game-client/Assets/Scripts/Test.cs
+++ game-client/Assets/Scripts/Test.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Globalization;
 using System.Text;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using DiscordGames.Core.Memory;
 using DiscordGames.Core.Memory.Pool;
+using DiscordGames.Core.Net;
+using DiscordGames.Core.Net.Message;
 using DiscordGames.Core.Net.Serialize;
 using TMPro;
 using UnityEngine;
@@ -16,18 +20,37 @@ public class Test : MonoBehaviour
         MemoryPool.Init(new SmallObjectHeapPool());
         
         var serialized =
-            MessageSerializer.WritePingMessage(
-                DiscordGames.Core.Net.MessageChannel.Direct, DateTime.UtcNow.Ticks);
+            MessageSerializer.WritePingMessage(MessageChannel.Direct, DateTime.UtcNow.Ticks);
 
         var str = string.Join(", ", serialized);
         sb.AppendLine($"<color=yellow>{str}</color>");
         
-        var reader = new BufferReader(serialized);
-        var header = reader.ReadHeader();
-        var message = reader.ReadPingMessage(ref header);
-        
-        sb.AppendLine(new DateTime(message.UtcTicks, DateTimeKind.Utc).ToLocalTime().ToString(CultureInfo.InvariantCulture));
+        MessageSerializer.Read(serialized, new TestHandler(GetComponent<TMP_Text>(), sb));
+    }
+    
+    private class TestHandler : IMessageHandler
+    {
+        private readonly TMP_Text text;
+        private readonly StringBuilder sb;
 
-        GetComponent<TMP_Text>().text = sb.ToString();
+        public TestHandler(TMP_Text text, StringBuilder sb)
+        {
+            this.text = text;
+            this.sb = sb;
+        }
+        
+        public ValueTask OnGreeting(GreetingMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask OnPing(PingMessage message)
+        {
+            sb.AppendLine(new DateTime(message.UtcTicks, DateTimeKind.Utc).ToLocalTime()
+                .ToString(CultureInfo.InvariantCulture));
+            text.text = sb.ToString();
+            return UniTask.CompletedTask.AsValueTask();
+        }
     }
 }
+
